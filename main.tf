bdbc39f0b21a823dc8c3252a227b1289b07cd78d
lakshmiprasad2019 <58114347+lakshmiprasad2019@users.noreply.github.com>
2021-12-19 16:07:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAWARYSVAQQCORH2FA"
-  secret_key = "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"
+  access_key = "AKIAWARYSVAQQIGRYFVR"
+  secret_key = "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"
 }
 resource "aws_instance" "myec2" {
   instance_type          = "t2.micro"
   ami                    = "ami-052cef05d01020f1d"
+  count  = 2
   
   tags = {
     Name = "import"
